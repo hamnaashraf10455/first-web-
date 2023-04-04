f52577698319c2a5afcaec4518d05abb1651a99d
hamnaashraf10455 <128869958+hamnaashraf10455@users.noreply.github.com>
2023-04-04 05:24:48 +0500

    web lab4

---
+++ sp20-bse-047-web_lab4/script.js
@@ -0,0 +1,11 @@
+let bar = document.getElementById("bar");
+
+
+
+window.onload=  function(){
+bar.onclick = function(){
+    navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("cc");
+}
+}
+
